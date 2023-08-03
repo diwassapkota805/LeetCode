ca49ffd9ae3a527911efba75c50c14d00c850786
Diwas Sapkota <102276270+diwassapkota805@users.noreply.github.com>
2023-08-03 15:43:37 +0000

    completed TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -1,1 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+Map<Integer, Integer> map = new HashMap<>();
 
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            // Check if the complement exists in the HashMap
+            if (map.containsKey(complement)) {
+                // Return the indices of the two numbers that add up to the target
+                return new int[]{map.get(complement), i};
+            }
+
+            // If the complement does not exist, store the current number and its index in the HashMap
+            map.put(nums[i], i);
+        }
+
+        // If no solution is found, return an empty array or throw an exception, depending on the requirements
+        return new int[]{};
+
+
+
+    }
+}
+// ARRAYS
