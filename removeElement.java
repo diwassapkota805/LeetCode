8010c2fc5eb716602d9245016aeb3d948a6b1b77
Diwas Sapkota <102276270+diwassapkota805@users.noreply.github.com>
2023-08-26 13:02:42 -0500

    27

---
+++ removeElement.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int j = 0;
+
+        while(i < nums.length)
+        {
+            if(nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+
+            i++;
+
+        }
+
+        // two poiners
+       
+        return j; 
+    }
+}
+
+// two pointers
+// 27
